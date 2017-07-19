eb52c267c75b405b343e26706e91641450e92fbd
ivansanchezvera <ivansanchez_1988@hotmail.com>
2017-07-19 11:14:22 -0500

    Agregado contenido super relevante a mi tesis.

--- src/AlgoritmoNuevo.java
+++ src/AlgoritmoNuevo.java
@@ -4,6 +4,26 @@ public class AlgoritmoNuevo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Algoritmo Salvaje aparece!");
+		
+		int i = 0;
+		int nota = 0 ;
+		boolean copiaEnElExamen = true;
+		do{
+			System.out.println("Tu nota es " + nota);
+			
+			if(i>5)
+			{
+				break;
+			}
+			i++;
+		}while(copiaEnElExamen);
+	}
+	
+	public static void imprimirTesis()
+	{
+		System.out.println("Blablabla relleno!!!");
+		System.out.println("Mas lata!");
+		System.out.println("Lata++");
 	}
 
 }
